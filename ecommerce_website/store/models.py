67a5aa614f7f9f5e5d98bdb7f5d75ce23312686d
Borislav Mutev <bmutev@gmail.com>
2024-01-21 18:48:58 +0200

    Category model

--- ecommerce_website/store/models.py
+++ ecommerce_website/store/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+
+    # db_index - for search acceleration and memory optimization
+    name = models.CharField(max_length=250, db_index=True)
+
+    # slug - used to get a particular category
+    # explanation: https://www.w3schools.com/django/django_slug_field.php
+    slug = models.SlugField(max_length=250, unique=True)
+
+    # Django visualizes the Category class in the Admin panel as Categorys.
+    # The meta configuration changes that.
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    # When a category is selected, the name returned from the databse is generic - Category(10, Category(20
+    # This changes the generic name to the name of the category
+    def __str__(self):
+        return self.name
