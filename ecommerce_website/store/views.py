2f0ac39ef9e9833b020ce456e0fd0488a1004b30
Borislav Mutev <bmutev@gmail.com>
2024-02-03 16:20:38 +0200

    Product cards now use data from DB

--- ecommerce_website/store/views.py
+++ ecommerce_website/store/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 
+from . models import Category, Product
 # Create your views here.
 
 
 def store(request):
-    return render(request, 'store/store.html')
+    all_product = Product.objects.all()
+
+    context = {'all_products': all_product}
+    return render(request, 'store/store.html', context)
+
+
+def categories(request):
+    all_categories = Category.objects.all()
+
+    return {'all_categories': all_categories}
