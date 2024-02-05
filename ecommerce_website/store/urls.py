a1bbaba916cbbf80947e2e77f8052bb1b6f5c7fd
Borislav Mutev <bmutev@gmail.com>
2024-02-05 18:53:54 +0200

    Create view and url path for product page

--- ecommerce_website/store/urls.py
+++ ecommerce_website/store/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     # Empty string because this is the homepage
     path('', views.store, name='store'),
+    path('product/<slug:slug>/', views.product_info, name='product-info'),
 ]
