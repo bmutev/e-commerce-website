f00dd589baf4de11e810bd3eb0a59d512a8f9a05
Borislav Mutev <bmutev@gmail.com>
2024-03-09 14:45:17 +0200

    Product prices are now calculated based on quantity

--- ecommerce_website/cart/cart.py
+++ ecommerce_website/cart/cart.py
@@ -47,3 +47,5 @@ class Cart:
             item['total'] = item['price'] * item['qty']
             yield item
 
+    def get_total(self):
+        return sum(Decimal(item['price']) * item['qty'] for item in self.cart.values())
